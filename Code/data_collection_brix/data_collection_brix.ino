4a3efcfdf1d92285f02115508dd763504641b88d
popofrank97 <popofrank97@gmail.com>
2019-04-24 01:36:30 -0700

    Added commends

--- Code/data_collection_brix/data_collection_brix.ino
+++ Code/data_collection_brix/data_collection_brix.ino
@@ -1,82 +1,100 @@
-
 /*
- * This program reads modbus data and stores in microSD card
- * 
- */
+   This code runs on Arduino Mega and collects data from two Brix sensor via one RS232
+   using Modbus protocol. It then saves the data to a csv file. The polling rate
+   can be changed.
+   The following modules are necessary to run this code:
+   RS 232 Shifter
+   RTC chip
+   SD Card module
+
+*/
+
+#include <RunningAverage.h>   // Necessary library to find running average of the values to prevent outliers
+#include <SD.h>               // Library for SD card module functions
+#include "RTClib.h"           // Access RTC functions
+#include <Wire.h>             // Communicate with I2C devices
+#include <ModbusMaster.h>     // Use for modbus protocol functions
 
 
- #include <RunningAverage.h>
- #include <ModbusMaster.h>
- #include <SD.h>
- #include "RTClib.h"
- #include <Wire.h>
+
+// Instantiate
+ModbusMaster node;            // Instantiate Modbus node
+RTC_PCF8523 rtc;              // Instantiate rtc module
+File myFile;                  // Instantiate File
+RunningAverage sensor1RA(20); // Instantiate RunningAverage
+RunningAverage sensor2RA(20);
 
 
-  ModbusMaster node;    
-  String dataString ="";
-  RTC_PCF8523 rtc;
-  File myFile;
-  String filename = "";
-  RunningAverage sensor1RA(20);
-  RunningAverage sensor2RA(20);
-  int samples = 0;
-  
+// Initialize
+String dataString = "";     // Initialize data string
+String filename = "";       // Initialize filename
+int samples = 0;            // Initialize sample
+
 void setup()
 {
-  
-  rtc.begin();
-  pinMode(53, OUTPUT);
-  SD.begin(53);
+
+  rtc.begin();        // Begin rtc module
+  pinMode(53, OUTPUT);// Set pin 53 to OUTPUT for SD card communication
+  SD.begin(53);       // Begin SD module
+  // Clear any values in runningAverage
   sensor1RA.clear();
   sensor2RA.clear();
-  
 
 
+  // the DateTime class now is the current time given by rtc module
   DateTime now = rtc.now();
 
-  Serial1.begin(9600);
-  Serial.begin(4800);
-  delay(100);  
+  Serial1.begin(9600);      // Modbus serial with baud rate 9600
+  Serial.begin(4800);       // Serial for debugging
+  delay(100);
   node.begin(1, Serial1);  // Modbus RTU
   delay(100);
-  Serial.println("Connected "); 
+  Serial.println("Connected ");
   Serial.println("Modbus RTU Master Online");
 
-//  dataString = String("Timestamp")+ "," + String("Sensor 1 Measured Pressure") +","+ String("Sensor1 Druck n. Lagekor") + "," + String("Sensor1 Sensor Pressure") + "," + String("Sensor1 temp")
-//   + "," + String("Sensor 2 Measured Pressure") +","+ String("Sensor2 Druck n. Lagekor") + "," + String("Sensor2 Sensor Pressure") + "," + String("Sensor2 temp");
-//
+  // Define csv filename format
   filename = String("data") + String(now.month()) + String(now.day()) + String(".csv");
 
+  // If the filename doesn't exist, create the header for CSV file. Header is the first row of CSV file
   if (!SD.exists(filename))
   {
     createHeader();
   }
-  
+
 }
 
-void createHeader(){
+/*
+   Function to create the header of CSV file.
+   It also updates the global filename value with current date
+*/
+void createHeader() {
   DateTime now = rtc.now();
-//   dataString = String("Timestamp")+ "," + String("Sensor 1 Measured Pressure") +","+ String("Sensor1 Druck n. Lagekor") + "," + String("Sensor1 Sensor Pressure") + "," + String("Sensor1 temp")
-//   + "," + String("Sensor 2 Measured Pressure") +","+ String("Sensor2 Druck n. Lagekor") + "," + String("Sensor2 Sensor Pressure") + "," + String("Sensor2 temp");
 
-  dataString = String("Timestamp")+ "," + String("Sensor 1 Measured Pressure") +","+  String("Sensor1 temp") + "," + String("Sensor 2 Measured Pressure") + "," + String("Sensor2 temp");
+  dataString = String("Timestamp") + "," + String("Sensor 1 Measured Pressure") + "," +  String("Sensor1 temp") + "," + String("Sensor 2 Measured Pressure") + "," + String("Sensor2 temp");
 
   filename = String("data") + String(now.month()) + String(now.day()) + String(".csv");
   saveData();
 }
 
 
-
-union ifloat 
+/*
+   Necessary to convert float to decimal
+*/
+union ifloat
 {
   uint8_t bytes[4];
   float val;
 };
 
+/*
+   @param reg : the modbus register the user wishes to retrive data from
+   This function reads the data on the modbus register as well as convert the value
+   from float to decimal.
+*/
 float getData(int reg)
 {
 
-  node.readHoldingRegisters(reg, 2); 
+  node.readHoldingRegisters(reg, 2);
 
   signed long node1 = node.getResponseBuffer(0);
   word node2 = node.getResponseBuffer(1);
@@ -90,7 +108,7 @@ float getData(int reg)
   f.bytes[2] = msb3;
   f.bytes[1] = msb2;
   f.bytes[0] = msb1;
-  
+
   node.clearResponseBuffer();
 
   return f.val;
@@ -99,52 +117,38 @@ float getData(int reg)
 
 void loop()
 {
-  
+
   DateTime now = rtc.now();
+  // update global filename
   filename = String("data") + String(now.month()) + String(now.day()) + String(".csv");
 
   if (!SD.exists(filename))
   {
     createHeader();
   }
-//  unsigned long seconds = 1000L; //Notice the L 
-//  unsigned long minutes = seconds * 60;
+
 
   int Mdelay = 3000;   // microdelay
 
-  Serial.print("Sensor 1 Measured Pressure: ");
+  Serial.print("Sensor 1 Measured Pressure: "); // Debug
   float f_1_1 = getData(256);
-  Serial.println(f_1_1,20 );
+  Serial.println(f_1_1, 20 );     // Debug
 
-//  Serial.print("Sensor1 Druck n. Lagekor: ");
-//  float f_1_2 = getData(258);
-//  Serial.println(f_1_2,20 );
-//
-//  Serial.print("Sensor1 Sensor Pressure:  ");
-//  float f_1_3 = getData(260);
-//  Serial.println(f_1_3,20 );
-
-  Serial.print("Sensor1 temp: ");
+  Serial.print("Sensor1 temp: "); // Debug
   float f_1_temp = getData(262);
-  Serial.println(f_1_temp, 20);
+  Serial.println(f_1_temp, 20);   // Debug
 
-  
-  Serial.print("Sensor 2 Measured Pressure: ");
+
+  Serial.print("Sensor 2 Measured Pressure: "); // Debug
   float f_2_1 = getData(264);
-  Serial.println(f_2_1,20 );
+  Serial.println(f_2_1, 20 );   // Debug
 
-//  Serial.print("Sensor1 Druck n. Lagekor: ");
-//  float f_2_2 = getData(266);
-//  Serial.println(f_2_2,20 );
-//
-//  Serial.print("Sensor1 Sensor Pressure:  ");
-//  float f_2_3 = getData(268);
-//  Serial.println(f_2_3,20 );
-  
-  Serial.print("Sensor2 temp: ");
+  Serial.print("Sensor2 temp: "); // Debug
   float f_2_temp = getData(270);
-  Serial.println(f_2_temp, 20);
+  Serial.println(f_2_temp, 20);   // Debug
 
+
+  // Retrieve time data
   String y = String(now.year());
   String mo = String(now.month());
   String d = String(now.day());
@@ -153,61 +157,61 @@ void loop()
   String m = String(now.minute());
   String s = String(now.second());
 
+  // Debug
   Serial.print(h);
   Serial.print(m);
   Serial.print(s);
   Serial.println();
 
+
+  // Concatenate time data
   String str = y + "/" + mo + "/" + d + " " + h + ":" + m + ":" + s;
 
+
+  // Push to runningAverage
   sensor1RA.addValue(f_1_1);
   sensor2RA.addValue(f_2_1);
-  Serial.println(samples);
+  Serial.println(samples);    // Debug
 
 
 
   samples++;
-  if (samples == 20)
+  if (samples == 20)      // Every minute, record the value given from runningAverage
   {
     samples = 0;
-//    dataString = str + "," + String(sensor1RA.getAverage(),20) + "," + String(f_1_2, 20) + "," + String(f_1_3, 20)+ ","+ String(f_1_temp, 20) + ","
-//      + String(sensor2RA.getAverage(),20) + "," + String(f_2_2, 20) + "," + String(f_2_3, 20)+ ","+ String(f_2_temp, 20);
-    
-    dataString = str + "," + String(sensor1RA.getAverage(),20)+ ","+ String(f_1_temp, 20) + "," + String(sensor2RA.getAverage(),20) + ","+ String(f_2_temp, 20);
-    
-    if (sensor1RA.getAverage() < 2.5 && sensor2RA.getAverage() < 2.5){
+
+    dataString = str + "," + String(sensor1RA.getAverage(), 20) + "," + String(f_1_temp, 20) + "," + String(sensor2RA.getAverage(), 20) + "," + String(f_2_temp, 20);
+
+    // Manually discard outliers
+    if (sensor1RA.getAverage() < 2.5 && sensor2RA.getAverage() < 2.5) {
       saveData();
     }
-    
+
     sensor1RA.clear();
     sensor2RA.clear();
-  Serial.println(sensor1RA.getAverage(),20);
-  Serial.println(sensor2RA.getAverage(),20);
+    // Debug
+    Serial.println(sensor1RA.getAverage(), 20);
+    Serial.println(sensor2RA.getAverage(), 20);
   }
-  
-  
-
-
-//  dataString = str + "," + String(f_1_1, 20) + "," + String(f_1_2, 20) + "," + String(f_1_3, 20)+ ","+ String(f_1_temp, 20) + ","
-//  + String(f_2_1, 20) + "," + String(f_2_2, 20) + "," + String(f_2_3, 20)+ ","+ String(f_2_temp, 20);
-//  saveData();
 
 
   delay(Mdelay);
-//  delay(minutes);
 
   Serial.println();
 
-  
+
 }
 
-void saveData(){
-  // now append new data fil
+
+/*
+   This function opens the csv file and adds a row of data
+*/
+void saveData() {
   myFile = SD.open(filename, FILE_WRITE);
 
   if (SD.exists(filename))
   {
-    if (myFile){
+    if (myFile) {
       myFile.println(dataString);
       myFile.close(); // close the file
     }
@@ -216,5 +220,5 @@ void saveData(){
   {
     Serial.println("Error writing to file !");
   }
-  
-  }
+
+}
--- Code/data_collection_redox/data_collection_redox.ino
+++ Code/data_collection_redox/data_collection_redox.ino
@@ -1,25 +1,42 @@
+/*
+   This code runs on Arduino Mega and collects data from a redox sensor via RS485
+   using Modbus protocol. It then saves the data to a csv file. The polling rate
+   can be changed.
+   The following modules are necessary to run this code:
+   RS485 to TTL
+   RTC chip
+   SD Card module
 
-#include <RunningAverage.h>
-#include <SD.h>
-#include "RTClib.h"
-#include <Wire.h>
+*/
 
-#include <ModbusMaster.h>
 
+#include <RunningAverage.h>   // Necessary library to find running average of the values to prevent outliers
+#include <SD.h>               // Library for SD card module functions
+#include "RTClib.h"           // Access RTC functions
+#include <Wire.h>             // Communicate with I2C devices
+#include <ModbusMaster.h>     // Use for modbus protocol functions
+
+
+// Rx/Tx is hooked up to Serial1 and Data Enable, Receive Enable are hooked up as follows:
 #define MAX485_DE   17
 #define MAX485_RE_NEG   16
 
-String dataString ="";
-RTC_PCF8523 rtc;
-File myFile;
-String filename = "";
-RunningAverage redoxRA(20);
+
+// Instantiate
+RTC_PCF8523 rtc;            // Instantiate rtc module
+File myFile;                // Instantiate File
+RunningAverage redoxRA(20);       // Instantiate RunningAverage
 RunningAverage temperatureRA(20);
+ModbusMaster node;          // Instantiate Modbus node
 
-int samples = 0;
 
-ModbusMaster node;
+// Initialize
+String dataString = "";     // Initialize data string
+String filename = "";       // Initialize filename
+int samples = 0;            // Initialize sample
 
+
+// Necessary enable functions before and after transmiaaion
 void preTransmission()
 {
   digitalWrite(MAX485_RE_NEG, 1);
@@ -37,39 +54,50 @@ void postTransmission()
 
 void setup() {
 
-  rtc.begin();
-  pinMode(53, OUTPUT);
-  SD.begin(53);
-  redoxRA.clear();
+  rtc.begin();        // Begin rtc module
+  pinMode(53, OUTPUT);// Set pin 53 to OUTPUT for SD card communication
+  SD.begin(53);       // Begin SD module
+  redoxRA.clear();    // Clear any values in the RunningAverage
   temperatureRA.clear();
-  
 
-  DateTime now = rtc.now();
-  
-  
+
+  DateTime now = rtc.now();     // the DateTime class now is the current time given by rtc module
+
+  // Set pinmodes for enable lines
   pinMode(MAX485_RE_NEG, OUTPUT);
   pinMode(MAX485_DE, OUTPUT);
 
+
+  // Manually write low to the enable lines
   digitalWrite(MAX485_RE_NEG, 0);
   digitalWrite(MAX485_DE, 0);
 
-  
+
   delay(100);
 
-  Serial1.begin(19200);
-  node.begin(1, Serial1);
+
+  Serial1.begin(19200);       // Modbus serial with baud rate 19200
+  node.begin(1, Serial1);     // Begin modbus in Serial1
   delay(100);
-  Serial.begin(9600);
+  Serial.begin(9600);         // Serial for debugging
+
   node.preTransmission(preTransmission);
   node.postTransmission(postTransmission);
 
+
+  // Hello world
   Serial.println("Goodnight moon!");
-  
+
   delay(100);
-  
-  filename = String("data") + String(now.month()) + 
-  String(now.day()) + String(".csv");
-  
+
+
+  // Define csv filename format
+  filename = String("data") + String(now.month()) +
+             String(now.day()) + String(".csv");
+
+
+
+  // If the filename doesn't exist, create the header for CSV file. Header is the first row of CSV file
   if (!SD.exists(filename))
   {
     createHeader();
@@ -78,28 +106,41 @@ void setup() {
 
 }
 
-void createHeader(){
+/*
+   Function to create the header of CSV file.
+   It also updates the global filename value with current date
+*/
+void createHeader() {
   DateTime now = rtc.now();
-  
-  dataString = String("Timestamp")+","+String("Redox value")+
-  ","+String("Temperature");
-  
-  filename = String("data") + String(now.month()) + 
-  String(now.day()) + String(".csv");
+
+  dataString = String("Timestamp") + "," + String("Redox value") +
+               "," + String("Temperature");
+
+  filename = String("data") + String(now.month()) +
+             String(now.day()) + String(".csv");
 
   saveData();
 }
 
-union ifloat 
+
+/*
+   Necessary to convert float to decimal
+*/
+union ifloat
 {
   uint8_t bytes[4];
   float val;
 };
 
+/*
+   @param reg : the modbus register the user wishes to retrive data from
+   This function reads the data on the modbus register as well as convert the value
+   from float to decimal.
+*/
 float getData(int reg)
 {
-  node.readHoldingRegisters(reg,10);
-  
+  node.readHoldingRegisters(reg, 10);
+
   signed long node1 = node.getResponseBuffer(3);
   word node2 = node.getResponseBuffer(2);
 
@@ -112,36 +153,40 @@ float getData(int reg)
   f.bytes[2] = msb3;
   f.bytes[1] = msb2;
   f.bytes[0] = msb1;
-  
-  
+
+
   node.clearResponseBuffer();
-  
+
   return f.val;
 }
 
 void loop() { // run over and over
 
   DateTime now = rtc.now();
-  
-  filename = String("data") + String(now.month()) + 
-  String(now.day()) + String(".csv");
-  
+  // update global filename
+  filename = String("data") + String(now.month()) +
+             String(now.day()) + String(".csv");
+
+
+  // same as in setup()
   if (!SD.exists(filename))
   {
     createHeader();
   }
-  
+
   int MDelay = 3000;
-  
+
+  // Debug
   Serial.print("Redox: ");
   float f_1 = getData(2089);
-  Serial.println(f_1,20);
-  
+  Serial.println(f_1, 20);
+
   Serial.print("Temperature: ");
   float f_2 = getData(2409);
   Serial.println(f_2, 20);
-  
-  
+
+
+  // Retrieve time data
   String y = String(now.year());
   String mo = String(now.month());
   String d = String(now.day());
@@ -150,50 +195,63 @@ void loop() { // run over and over
   String m = String(now.minute());
   String s = String(now.second());
 
+
+  // Debug
   Serial.print(h);
   Serial.print(m);
   Serial.print(s);
   Serial.println();
-  
+
+
+  // Concatenate time data
   String str = y + "/" + mo + "/" + d + " " + h + ":" + m + ":" + s;
-  
+
+
+  // Push to runningAverage
   redoxRA.addValue(f_1);
   temperatureRA.addValue(f_2);
-  Serial.println(samples);
+
+  Serial.println(samples);      // debug
   samples++;
-  
-  if (samples == 300)
+
+  if (samples == 300)     // Every 15 minutes, record the value given from runningAverage
   {
     samples = 0;
-    dataString = str+","+String(redoxRA.getAverage(),20)+
-    ","+String(temperatureRA.getAverage(),20);
-    
-    
-    if (redoxRA.getAverage() < 3000 && temperatureRA.getAverage() < 100){
+    dataString = str + "," + String(redoxRA.getAverage(), 20) +
+                 "," + String(temperatureRA.getAverage(), 20);
+
+    // Manually discard outliers
+    if (redoxRA.getAverage() < 3000 && temperatureRA.getAverage() < 100) {
       saveData();
     }
-    
+
     redoxRA.clear();
     temperatureRA.clear();
-    Serial.println(redoxRA.getAverage(),20);
-    Serial.println(temperatureRA.getAverage(),20);
+
+    // Debug
+    Serial.println(redoxRA.getAverage(), 20);
+    Serial.println(temperatureRA.getAverage(), 20);
   }
-  
+
   delay(MDelay);
   Serial.println();
 
 }
 
-  void saveData(){
+
+/*
+   This function opens the csv file and adds a row of data
+*/
+void saveData() {
 
   myFile = SD.open(filename, FILE_WRITE);
-  
+
   Serial.println(myFile);
   Serial.println(filename);
-  
+
   if (SD.exists(filename))
   {
-    if (myFile){
+    if (myFile) {
       myFile.println(dataString);
       myFile.close(); // close the file
     }
@@ -202,9 +260,5 @@ void loop() { // run over and over
   {
     Serial.println("Error writing to file !");
   }
-  
-}
-  
 
-  
-  
+}
